64de2df3c7a07a178427826f16ba542db47baa18
Tim Konick <konick781@gmail.com>
2024-03-01 05:52:17 -0500

    Re-Factor unit-test

--- Test/runner.py
+++ Test/runner.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from pathlib import *
+from typing import Callable
+
 import pymysql
 import pytest
 import random
 
 
-def mysql_connect():
-    global data_hash
-    global insert_data
+def word_generator(fp: PosixPath) -> Iterator[str]:
+    scrub: Callable[[str], str] = lambda s: s.strip('\n')
+    with open(fp) as fh:
+        for datum in map(scrub, fh.readlines()):
+            yield datum
+
+def mysql_connect(wgen: Iterator[str]) -> pymysql.connection:
     connection = pymysql.connect(
         unix_socket='/tmp/mysql.sock',
         user='tijko',
@@ -19,20 +26,22 @@ def mysql_connect():
     )
     cursor = connection.cursor()
     cursor.execute("CREATE TABLE IF NOT EXISTS my_table(table_id INT PRIMARY KEY AUTO_INCREMENT, table_name VARCHAR(64) NOT NULL, table_number INT NOT NULL)")
-    query = "INSERT INTO my_table(table_name, table_number) VALUES (%s, %s)"
-    picks = 0
+    # Init stack variables
+    picks: int = 0
+    query: str = f"INSERT INTO my_table(table_name, table_number) VALUES (%s, %s)"
+    member_query: str = f"SELECT * FROM my_table WHERE table_name = %s"
+    delete_q: str = f"DELETE FROM my_table WHERE table_name = %s"
     # Load data
-    data_limit = 5001
-    insert_data = dict()
+    data_limit: int = 5001
+    insert_data: dict = {}
     # Arbitary table-data from dictionary
-    data_hash = {v:k for k,v in enumerate(raw_data, 1)}
+    data_hash = {v:k for k,v in enumerate(wgen, 1)}
     while picks < data_limit:
         # random data
         item_idx = random.randint(0, 466550)
         if insert_data.get(item_idx):
             continue
         # Check if test-db has data-item....
-        member_query = f"SELECT * FROM my_table WHERE table_name = %s"
         cursor = connection.cursor()
         item = raw_data[item_idx]
         cursor.execute(member_query, (item,))
@@ -43,7 +52,6 @@ def mysql_connect():
                 cursor.close()
                 cursor = connection.cursor()
                 continue
-            delete_q = f"DELETE FROM my_table WHERE table_name = %s"
             cursor.execute(delete_q, (item,))
             connection.commit()
             cursor.close()
@@ -61,7 +69,7 @@ def mysql_connect():
 
 def run_test(connection):
     cursor = connection.cursor()
-    query = "SELECT * FROM my_table WHERE table_number = %s"
+    query: str = f"SELECT * FROM my_table WHERE table_number = %s"
     for _ in range(20):
         data_number = random.choice(list(insert_data.keys()))
         data = raw_data[data_number]
@@ -74,20 +82,20 @@ def run_test(connection):
         print('Hit a match for....{}'.format(data))
 
 def duplicate_finder(connection):
-    cursor = connection.cursor()
-    db_dump_q = f"SELECT * FROM my_table"
+    cursor  = connection.cursor()
+    db_dump_q: str = f"SELECT * FROM my_table"
     cursor.execute(db_dump_q)
     data = cursor.fetchall()
-    dump = []
+    dump: list = []
     for i in data:
         if i not in dump:
             dump.append(i)
-    print(dump == data)
+    assert dump == data
 
 if __name__ == '__main__':
-    global raw_data
-    with open('test-data.txt') as fh:
-        raw_data = [s.strip('\n') for s in fh.readlines()]
-    data_hash = {}
-    connection = mysql_connect()
+    fpath: PosixPath = PosixPath('../Test/test-data.txt')
+    word_gen = word_generator(fpath)
+    data_hash: dict = {}
+    raw_data: dict = {}
+    connection = mysql_connect(word_gen)
     duplicate_finder(connection)
